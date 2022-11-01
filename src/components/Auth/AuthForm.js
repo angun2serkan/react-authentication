c70077a7fea7c842d4f857efe7ebcb0e0cc0ff68
Ethem Serkan Angün <serkan.angun@ecospend.com>
2022-11-01 11:50:46 +0300

    using token and redirect user

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,12 +1,14 @@
 import { useState, useRef, useContext } from 'react';
-import AuthContextProvider from '../../store/auth-context';
+import {useHistory} from 'react-router-dom'
+import AuthContext from '../../store/auth-context';
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
+  const history = useHistory();
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
-  const authCtx = useContext(AuthContextProvider);
+  const authCtx = useContext(AuthContext);
 
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
@@ -21,50 +23,50 @@ const AuthForm = () => {
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
 
+    // optional: Add validation
 
     setIsLoading(true);
     let url;
-
-    if(isLogin){
-      url='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCHo5QxsC49-Ihs_vmTgng56lPVwwbXyA4';
-    }else{
-      url='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCHo5QxsC49-Ihs_vmTgng56lPVwwbXyA4';
+    if (isLogin) {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCHo5QxsC49-Ihs_vmTgng56lPVwwbXyA4';
+    } else {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCHo5QxsC49-Ihs_vmTgng56lPVwwbXyA4';
     }
-
-    fetch(url,
-    {
+    fetch(url, {
       method: 'POST',
       body: JSON.stringify({
         email: enteredEmail,
         password: enteredPassword,
-        retrunSecureToken: true
+        returnSecureToken: true,
       }),
       headers: {
-        'Content-Type': 'application/json'
-      }
-    }
-    ).then(res => {
-      setIsLoading(false);
-      if(res.ok){
-        return res.json();
-      }else {
-        return res.json().then(data => {
-          // show an werror modal
-          let errorMessage = 'Authentication failed!';
-          if(data && data.error && data.error.message){
-            errorMessage = data.error.message;
-          }
-          alert(errorMessage);
-          throw new Error(errorMessage);
-        });
-      }
+        'Content-Type': 'application/json',
+      },
     })
-    .then(data => {
-      authCtx.login(data.idToken);
-    })
-    .catch(err => {
-      alert(err.message);
-    });
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = 'Authentication failed!';
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
+
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        authCtx.login(data.idToken);
+        history.replace("/");
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
   };
 
   return (
@@ -73,14 +75,21 @@ const AuthForm = () => {
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required ref={emailInputRef}/>
+          <input type='email' id='email' required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required ref={passwordInputRef} />
+          <input
+            type='password'
+            id='password'
+            required
+            ref={passwordInputRef}
+          />
         </div>
         <div className={classes.actions}>
-          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {!isLoading && (
+            <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          )}
           {isLoading && <p>Sending request...</p>}
           <button
             type='button'
